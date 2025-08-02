9f82fae372f7c1c49c72072dd3e57e8212aed660
jxsoren <jxsoren@gmail.com>
2025-08-02 16:05:44 -0600

    add candy bucket to collect and track count of each candy

---
+++ src/CandyBucket.java
@@ -0,0 +1,40 @@
+import java.util.EnumMap;
+import java.util.Map;
+
+/**
+ * Represents a trick-or-treater's candy bucket for collecting Halloween candy.
+ * Tracks the quantity of each candy type collected during trick-or-treating.
+ *
+ * @author Josh Sorensen
+ */
+
+public class CandyBucket {
+    private final Map<CandyType, Integer> candyStash;
+
+    public CandyBucket() {
+        this.candyStash = new EnumMap<>(CandyType.class);
+        initializeCandyStash();
+    }
+
+    public int getCandyQuantity(CandyType candyType) {
+        return candyStash.get(candyType);
+    }
+
+    public void addCandy(CandyType candyType) {
+        int candyQuantity = candyStash.get(candyType);
+        candyStash.put(candyType, candyQuantity + 1);
+    }
+
+    public void removeCandy(CandyType candyType) {
+        int candyQuantity = candyStash.get(candyType);
+        if (candyQuantity > 0) {
+            candyStash.put(candyType, candyQuantity - 1);
+        }
+    }
+
+    private void initializeCandyStash() {
+        for (CandyType type : CandyType.values()) {
+            candyStash.put(type, 0);
+        }
+    }
+}
